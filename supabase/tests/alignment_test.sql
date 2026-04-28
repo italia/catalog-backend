99e9e1bec115b9d2f81ef2d96b0e6248978be719
Nicola Squartini <tensor5@gmail.com>
2026-04-28 13:25:33 +0200

    test: check SQL column alignment

---
+++ supabase/tests/alignment_test.sql
@@ -0,0 +1,24 @@
+BEGIN;
+SELECT
+    plan (3);
+SELECT
+    results_eq ($$
+        SELECT
+            pg_column_size(ROW ('00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', 'epoch', 'https://github.com/italia', TRUE)::code_hostings) $$, $$
+        VALUES (91) $$, 'Size of a “code_hostings” row');
+SELECT
+    results_eq ($$
+        SELECT
+            pg_column_size(ROW ('00000000-0000-0000-0000-000000000000', 'epoch', 'test@example.com', 'foo', 'pcm', 'https://example.com', TRUE)::publishers) $$, $$
+        VALUES (94) $$, 'Size of a “publishers” row');
+SELECT
+    results_eq ($$
+        SELECT
+            pg_column_size(ROW ('00000000-0000-0000-0000-000000000000', 'epoch', 'https://github.com/italia/foo', '{"publiccodeYmlVersion":"0.5"}', TRUE)::softwares) $$, $$
+        VALUES (115) $$, 'Size of a “software” row');
+SELECT
+    *
+FROM
+    finish ();
+ROLLBACK;
+
